12379998c788acba749b3fc063f75135506ee89a
morph1nX <rhythmaich@gmail.com>
2022-12-29 17:10:52 +0530

    1. admin dashboard added 2. new routes for flights, bookings and passengers added

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -2,7 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
-import AdminDashboard from "../pages/AdminDashboard";
+import Dashboard from "../pages/admin/Dashboard";
+import FlightManagement from "../pages/admin/Flights"
+import Passengers from "../pages/admin/Passengers";
+import Bookings from "../pages/admin/Bookings"
 
 const Routing = () => {
   return (
@@ -12,7 +15,10 @@ const Routing = () => {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/admindashboard" element={<AdminDashboard />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/flightmanagement" element={<FlightManagement />} />
+          <Route path="/bookings" element={<Bookings />} />
+          <Route path="/passengers" element={<Passengers />} />
           {/* <Route path="/" element={<Home />} /> */}
         </Routes>
       </BrowserRouter>
