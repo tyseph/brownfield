bc63c85ef73b20bbef4f5447e9e55e4393db8651
Manikwar <sangameshwar-dnyaneshwar.manikwar@capgemini.com>
2023-02-09 20:45:06 +0530

    Environment Variable Added

--- src/api/URL.js
+++ src/api/URL.js
@@ -1,13 +1,18 @@
-const hostname = `LIN51016635`
+// const hostname = `LIN51016635`
 
 
-export const authURL = `http://${hostname}.corp.capgemini.com:8089/home`
-export const flightManagementURL = `http://${hostname}.corp.capgemini.com:8089/search`
-export const bookingManagementURL = `http://${hostname}.corp.capgemini.com:8089/booking`
-export const userDetailsURL = `http://${hostname}.corp.capgemini.com:8089/home`
+// export const authURL = `http://${hostname}.corp.capgemini.com:8089/home`
+// export const flightManagementURL = `http://${hostname}.corp.capgemini.com:8089/search`
+// export const bookingManagementURL = `http://${hostname}.corp.capgemini.com:8089/booking`
+// export const userDetailsURL = `http://${hostname}.corp.capgemini.com:8089/home`
 
 
 // http://${hostname}:8090/search
 
 // http://http://${hostname}:8089/home/getAllUser
+
+export const authURL = `${process.env.REACT_APP_URL}/home`
+export const flightManagementURL = `${process.env.REACT_APP_IP}/search`
+export const bookingManagementURL = `${process.env.REACT_APP_IP}/booking`
+export const userDetailsURL = `${process.env.REACT_APP_IP}/home`
 
