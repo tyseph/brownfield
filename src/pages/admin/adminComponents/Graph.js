97a022d55e145eb4df09cc0b0fd3567abc7c86c4
Sangameshwar18 <73937300+Sangameshwar18@users.noreply.github.com>
2023-02-12 17:22:46 +0530

    Updated Graph.js
    1. On hover gets correct total revenue

--- src/pages/admin/adminComponents/Graph.js
+++ src/pages/admin/adminComponents/Graph.js
@@ -32,18 +32,22 @@ const Graph = ({ revenue, graph }) => {
   });
   // console.log(graphData)
 
-  const CustomTooltip = () => (
-    < div className="rounded-xl overflow-hidden tooltip-head" >
-      <div className="flex items-center justify-between p-2">
-        <div className="">Total Revenue</div>
-        <Icon path="res-react-dash-options" className="w-2 h-2" />
-      </div>
-      <div className="tooltip-body text-center p-3">
-        <div className="text-white font-bold">{revenue}</div>
-        {/* <div className="">Revenue from 230 sales</div> */}
-      </div>
-    </div >
-  );
+  const CustomTooltip = ({ active, payload }) => {
+    if (active) {
+      return (
+        < div className="rounded-xl overflow-hidden tooltip-head" >
+          <div className="flex items-center justify-between p-2">
+            <div className="">Total Revenue</div>
+            <Icon path="res-react-dash-options" className="w-2 h-2" />
+          </div>
+          <div className="tooltip-body text-center p-3">
+            <div className="text-white font-bold">{payload[0].payload.totalRevenue}</div>
+            {/* <div className="">Revenue from 230 sales</div> */}
+          </div>
+        </div >
+      )
+    }
+  };
 
 
   return (
